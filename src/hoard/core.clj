84cd39eb2006d66ef5a0f3e60f50a0304a5d4b9d
faiz <faiz@helpshift.com>
2021-01-11 11:46:22 +0530

    Bulk processor implementation

--- src/hoard/core.clj
+++ src/hoard/core.clj
@@ -1,6 +1,57 @@
-(ns hoard.core)
+(ns hoard.core
+  (:require [clojure.core.async :refer [<!! chan >!! go-loop timeout alts! >! thread close!]]))
 
-(defn foo
-  "I don't do a whole lot."
-  [x]
-  (println x "Hello, World!"))
+
+(defprotocol IBulkProcessor
+  (-close [this])
+  (-flush [this])
+  (-add [this request]))
+
+
+(defmacro exec-with-limiter
+  [limiter & body]
+  `(do
+     (>! ~limiter :exec)
+     (thread
+       (try
+         ~@body
+         (finally
+           (<!! ~limiter))))))
+
+
+(defn bulk-processor
+  [f blimit btimeout bconcurrency]
+  (let [requests-ch (chan)
+        controller-ch (chan)
+        rate-limiter (chan bconcurrency)]
+    (go-loop [acc [] ccount 0 timer nil]
+      (let [chs (concat [requests-ch controller-ch]
+                        (when timer
+                          [timer]))
+            [v _] (alts! chs)]
+        (if v
+          (case v
+            :close (when (seq acc)
+                     (exec-with-limiter rate-limiter (f acc)))
+            :flush (do
+                     (when (seq acc)
+                       (exec-with-limiter rate-limiter (f acc)))
+                     (recur [] 0 nil))
+            (let [acc (conj acc v)]
+              (if (<= blimit (+ 1 ccount))
+                (do (exec-with-limiter rate-limiter (f acc))
+                    (recur [] 0 nil))
+                (recur acc (inc ccount) (or timer (timeout btimeout))))))
+          ;; timeout
+          (when (seq acc)
+            (exec-with-limiter rate-limiter (f acc))
+            (recur [] 0 nil)))))
+    (reify BProcessor
+      (-close [_]
+        (close! requests-ch)
+        (>!! controller-ch :close)
+        (close! controller-ch))
+      (-flush [_]
+        (>!! controller-ch :flush))
+      (-add [_ req]
+        (>!! requests-ch req)))))
